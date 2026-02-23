0d9fa00a22ee1fc35e030bb9a76a00d6a632e9b2
Raphael Morales-Teillac <rapha0731@gmail.com>
2026-02-23 22:30:25 +0100

    fix

--- main.py
+++ main.py
@@ -17,63 +17,83 @@ def tirer_carte(paquet):
 
 def calculer_score(main):
     total = 0
-    for i in main:
-        if main [i] == "J" or main [i] == "Q" or main [i] == "K":
-            total + 10
-        elif main [i] == "A":
+    for carte in main:
+        rang = carte[0]
+        if rang == "J" or rang == "Q" or rang == "K":
+            total = total + 10
+        elif rang == "A":
             if total + 11 > 21:
-                total - 10
+                total = total + 1
         else :
-            total + main [i]
+            total = total + int(rang)
     return total
 
 def afficher_main(nom, main, cacher_premiere=False):
-    print(nom," :")
-    if cacher_premiere == True:
-        print("Premiere carte caché !")
+    print(f"{nom} :")
+    if cacher_premiere:
+        print("Carte cachée")
         for i in range(1, len(main)):
-            print (main[i])
+            rang, couleur = main[i]
+            print(f"{rang} de {couleur}")
     else:
-        for i in main:
-            print (main[i])
+        for rang, couleur in main:
+            print(f"{rang} de {couleur}")
+
 
 
 def tour_joueur(paquet, main_joueur, main_croupier):
+    tirage = True
     while tirage == True:
-        print("Voici votre main :", afficher_main("Joueur",main_joueur))
-        clavier = input("tirer ou rester ?")
-        if clavier == "tirer":
-            main_joueur.append(tirer_carte(paquet))
-        elif clavier == "rester":
+        if calculer_score(main_joueur) > 21:
             break
-        else: 
-            print("Erreur ")
-
-
-
-
-
-
-
-
-
+        else:
+            afficher_main("Joueur",main_joueur)
+            afficher_main("Croupier",main_croupier, True)
+            
+            clavier = input("tirer ou rester ?")
+            if clavier == "tirer":
+                main_joueur.append(tirer_carte(paquet))
+            elif clavier == "rester":
+                break
+            else: 
+             print("Erreur ")
+    
         
+def tour_croupier(paquet, main_croupier):
+    while calculer_score(main_croupier) < 17:
+        main_croupier.append(tirer_carte(paquet))
 
+def determiner_gagnant(score_joueur, score_croupier):
+    if score_croupier > 21:
+        return "joueur"
+    elif score_croupier > score_joueur:
+        return "croupier"
+    elif score_joueur > 21:
+        return "croupier"
+    elif score_croupier == score_joueur:
+        return "egalité"
+    elif score_joueur > score_croupier:
+        return "joueur"
+    
+def jouer_manche():
+    paquet = []
+    main_croupier = []
+    main_joueur = []
 
+    paquet = creer_paquet()
 
+    main_croupier.append(tirer_carte(paquet))
+    main_joueur.append(tirer_carte(paquet))
+    main_croupier.append(tirer_carte(paquet))
+    main_joueur.append(tirer_carte(paquet))
 
+    tour_joueur(paquet, main_joueur, main_croupier)
+    tour_croupier(paquet, main_croupier)
 
+    gagnant = determiner_gagnant(calculer_score(main_joueur), calculer_score(main_croupier))
 
+    print("le gagnant est le : ",gagnant)
 
 
 # main
-paquet = creer_paquet()
-
-for c in paquet:
-    print(c)
-
-print(len(paquet))
-
-tirage = tirer_carte(paquet)
-
-print(tirage)
+jouer_manche()
